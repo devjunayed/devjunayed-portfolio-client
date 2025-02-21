5a66ec932de4761d8e8cc992131868f058a8e82e
devjunayed <devjunayed@gmail.com>
2025-02-21 17:37:47 +0600

    cleanup

--- tailwindcss.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "tailwindcss/lib/util/flattenColorPalette" {
-    const flattenColorPalette: (colors: object) => object;
-    export default flattenColorPalette;
-  }
-
-declare module "react-infinite-scroller"
-declare module 'react-code-blocks'
