764115a8c2a037a286d2548eef87ab8fa35681f5
devjunayed <devjunayed@gmail.com>
2025-02-25 21:50:31 +0600

    contact us page created

---
+++ src/app/(normal)/(home)/components/ContactUs/ContactUs.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactUs = () => {
+  return (
+    <div>
+        {/* <Heading title="Contact Us" /> */}
+    </div>
+  )
+}
+
+export default ContactUs
--- src/app/(normal)/(home)/page.tsx
+++ src/app/(normal)/(home)/page.tsx
@@ -4,15 +4,17 @@ import Skills from "@/app/(normal)/(home)/components/Skills";
 import { TracingBeam } from "@/components/ui/tracing-beam";
 import React from "react";
 import { Projects } from "./components/Projects";
+import ContactUs from "./components/ContactUs/ContactUs";
 
 const page = () => {
   return (
     <div className="bg-slate-900 mt-20 ">
-      <TracingBeam className="">
+      <TracingBeam className="h-full">
         <Hero />
         <Education />
         <Skills />
         <Projects />
+        <ContactUs />
       </TracingBeam>
     </div>
   );
