c3b068f1bdf4a582bfd07fe92a1cdbb32abb1d91
devjunayed <devjunayed@gmail.com>
2025-02-25 21:59:25 +0600

    single product page created

--- src/app/(normal)/projects/[projectId]/page.tsx
+++ src/app/(normal)/projects/[projectId]/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const SingleProjectPage = () => {
   return (
-    <div>
-      single project page
+    <div className="mt-20  max-w-6xl mx-auto   text-white">
+      <div className="mx-6">single project page</div>
     </div>
-  )
-}
+  );
+};
 
-export default SingleProjectPage
+export default SingleProjectPage;
