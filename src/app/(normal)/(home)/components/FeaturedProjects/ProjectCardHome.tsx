5d1b26fa90ab443bcb32a83eab789acc1a194db3
Md Junayed <devjunayed@gmail.com>
2025-05-10 16:29:37 +0600

    project card image on hover scroll add

--- src/app/(normal)/(home)/components/FeaturedProjects/ProjectCardHome.tsx
+++ src/app/(normal)/(home)/components/FeaturedProjects/ProjectCardHome.tsx
@@ -64,16 +64,17 @@ const ProjectCardHome = ({ project }: { project: TProjectData }) => {
               setIsModalOpen={setIsSingleOpen}
             >
               <div className="h-[96vh] overflow-y-scroll">
-                <div className="w-full flex justify-center ">
-                  <Image
-                    className="w-full mx-auto "
-                    alt=""
-                    src={project.projectThumbnail}
-                  />
-                </div>
+                <div
+                  className={`w-full bg-cover bg-top  relative h-[200px] md:h-[470px] transition-all   hover:bg-bottom linear  bg-image-scroll`}
+                  style={{
+                    backgroundImage: `url(${project.projectThumbnail})`,
+                  }}
+                ></div>
                 <div className="md:mt-4 mt-2 justify-center flex flex-wrap gap-1 md:gap-2">
                   {project.projectTags.map((tag, index) => (
-                    <Chip className="bg-white" key={`tag${index}`}>{tag}</Chip>
+                    <Chip className="bg-white" key={`tag${index}`}>
+                      {tag}
+                    </Chip>
                   ))}
                 </div>
                 <div className="flex gap-4 my-6 items-center justify-center">
